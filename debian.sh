9765e93c7aacb74e74555f6acf79793260f0830f
cmotc <cmotc@openmailbox.org>
2016-01-04 04:31:00 -0500

    removed old test.sh file

--- debian.sh
+++ debian.sh
@@ -38,10 +38,10 @@ echo bin/$SOURCEBIN usr/bin > debian/install
 for d in share/lair/*; do
     if [ -d $d ]; then
         for e in $d/*; do
-            echo $e >> debian/install
+            echo usr/$e >> debian/install
         done
     else
-        echo $d >> debian/install
+        echo usr/$d >> debian/install
     fi
 done
 echo $SOURCEDOC usr/share/doc/$DEBFOLDER >> debian/install
--- test.sh
+++
@@ -1,9 +0,0 @@
-for d in share/lair/*; do
-    if [ -d $d ]; then
-        for e in $d/*; do
-            echo $e #>> debian/install
-        done
-    else
-        echo $d #>> debian/install
-    fi
-done
