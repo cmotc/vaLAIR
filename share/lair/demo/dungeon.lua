bffb5d136442a212a9c8fec32435d0b50180d7cd
idk <idk@fireaxe.local>
2017-01-11 07:54:08 -0500

    changed demo/dungeon.lua

--- share/lair/demo/dungeon.lua
+++ share/lair/demo/dungeon.lua
@@ -4,61 +4,27 @@ dofile("/usr/share/lair/lua/common.lua")
 --
 function map_cares_insert()
         local decided_to = "false"
-        print("  Generator is at Coarse X: " .. generator_coarse_x.x .. " in the room")
-        print("  Generator is at Coarse Y: " .. generator_coarse_y.y .. " in the room")
-        print("  Generator is at Coarse X: " .. lua_get_x() .. " on the floor")
-	print("  Generator is at Coarse Y: " .. lua_get_y() .." on the floor")
-        print("  Generator Segment Coarse Width: " .. generator_coarse_w.w)
-	print("  Generator Segment Coarse Height: " .. generator_coarse_h.h)
-	print("  Generator is at X: " .. generator_x.x)
-	print("  Generator is at Y: " .. generator_y.y)
-        print("  Segment Starts at Coarse X: " .. room_coarse_x.x)
-	print("  Segment Starts at Coarse Y: " .. room_coarse_y.y)
-        print("  Segment Ends at Coarse X+Width: " .. room_coarse_xw.x)
-	print("  Segment Ends at Coarse Y+Height: " .. room_coarse_yh.y)
-        print("  Segment Starts at X: " .. room_x.x)
-	print("  Segment Starts at Y: " .. room_y.y)
-        print("  Segment Ends at X+Width: " .. room_xw.x)
-	print("  Segment Ends at Y+Height: " .. room_yh.y)
-        print("  Generator Segment Height: " .. generator_w.w)
-	print("  Generator Segment Width: " .. generator_h.h)
-        print("  Generator Particle Count: " .. generator_particle_count.c)
-	print("  Generator Mobile Count: " .. generator_mobile_count.c)
-        print_tag_count(wall)
-        --print("  Generator Particle Count: " .. generator_particle_count.c)
-        if generator_coarse_x.x < 2 then
-                if generator_coarse_x.x < 7 then
-                        decided_to="true";
-                elseif generator_coarse_x.x > 8 then
-                        decided_to="true";
-                else
+        print_props()
+        if lua_get_x() < 3 then
+                decided_to="true"
+        elseif lua_get_x() > (room_coarse_xw.x - 3) then
+                decided_to="true"
+        end
+        if lua_get_y() < 3 then
+                decided_to="true"
+        elseif lua_get_y() > (room_coarse_yh.y - 3) then
+                decided_to="true"
+        end
+        if generator_coarse_x.x > 5 then
+                if generator_coarse_x.x < 9 then
+                        print("x " .. generator_coarse_x.x .. " " .. 5 .. " " .. 9)
                         decided_to="false"
                 end
         end
-        if generator_coarse_x.x > generator_coarse_w.w - 3 then
-                if generator_coarse_x.x < 7 then
-                        decided_to="true";
-                elseif generator_coarse_x.x > 8 then
-                        decided_to="true";
-                else
-                        decided_to="false"
-                end
-        end
-        if generator_coarse_y.y < 4 then
-                if generator_coarse_y.y < 7 then
-                        decided_to="true";
-                elseif generator_coarse_y.y > 8 then
-                        decided_to="true";
-                else
-                        decided_to="false"
-                end
-        end
-        if generator_coarse_y.y > generator_coarse_h.h - 5 then
-                if generator_coarse_y.y < 7 then
-                        decided_to="true";
-                elseif generator_coarse_y.y > 8 then
-                        decided_to="true";
-                else
+
+        if generator_coarse_y.y > 5 then
+                if generator_coarse_y.y < 9 then
+                        print("y " .. generator_coarse_y.y .. " " .. 5 .. " " .. 9)
                         decided_to="false"
                 end
         end
--- share/lair/lua/common.lua
+++ share/lair/lua/common.lua
@@ -1,3 +1,4 @@
+math.randomseed(os.time())
 
 function lua_get_x()
         return tonumber(generator_coarse_x.x) + tonumber(room_coarse_x.x)
@@ -21,4 +22,28 @@ function print_tag_count(variable)
         else
                 return "0"
         end
+end
+
+
+function print_props()
+        print("  Generator is at Coarse X: " .. generator_coarse_x.x .. " in the room")
+        print("  Generator is at Coarse Y: " .. generator_coarse_y.y .. " in the room")
+        print("  Generator is at Coarse X: " .. lua_get_x() .. " on the floor")
+	print("  Generator is at Coarse Y: " .. lua_get_y() .." on the floor")
+        print("  Generator Segment Coarse Width: " .. generator_coarse_w.w)
+	print("  Generator Segment Coarse Height: " .. generator_coarse_h.h)
+	print("  Generator is at X: " .. generator_x.x)
+	print("  Generator is at Y: " .. generator_y.y)
+        print("  Segment Starts at Coarse X: " .. room_coarse_x.x)
+	print("  Segment Starts at Coarse Y: " .. room_coarse_y.y)
+        print("  Segment Ends at Coarse X+Width: " .. room_coarse_xw.x)
+	print("  Segment Ends at Coarse Y+Height: " .. room_coarse_yh.y)
+        print("  Segment Starts at X: " .. room_x.x)
+	print("  Segment Starts at Y: " .. room_y.y)
+        print("  Segment Ends at X+Width: " .. room_xw.x)
+	print("  Segment Ends at Y+Height: " .. room_yh.y)
+        print("  Generator Segment Height: " .. generator_w.w)
+	print("  Generator Segment Width: " .. generator_h.h)
+        print("  Generator Particle Count: " .. generator_particle_count.c)
+	print("  Generator Mobile Count: " .. generator_mobile_count.c)
 end
